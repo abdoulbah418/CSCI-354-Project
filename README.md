0eb649d8f4f0b375a9c063a30a03386ccebea71c
abdoulbah418 <69488874+abdoulbah418@users.noreply.github.com>
2021-11-17 13:07:17 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
 
 
-python3 life_manager/main.py --ics ./life.ics classes add --name="csci 354" --begin="2021-11-08 09:00:00" --end="2021-11-08 10:00:00"
-python3 life_manager/main.py --ics ./life.ics classes show
-python3 life_manager/main.py --ics ./life.ics classes remove --uid="703c34cf-d681-14271-bd6b-24ab541e3a8a@703c.org"
 
-python3 life_manager/main.py --ics ./life.ics reminders show
-python3 life_manager/main.py --ics ./life.ics reminders add --name="Drink Water" --time="2021-11-08 08:00:00"
-python3 life_manager/main.py --ics ./life.ics reminders complete --uid 48aefcff-30b4-4f31-85a1-93692e8fbd5c
+pyhton3 main.py --help
 
+python3 main.py --ics ./life.ics events add --name="csci 354" --begin="2021-11-08 09:00:00" --end="2021-11-08 10:00:00"
+python3 main.py --ics ./life.ics events show
+python3 main.py --ics ./life.ics events remove --uid="703c34cf-d681-4271-bd6b-24ab541e3a8a@703c.org"
 
-libraries used:
-click | https://click.palletsprojects.com/en/8.0.x/
-ics | https://pypi.org/project/ics/
-prettytable | https://pypi.org/project/prettytable/
+python3 main.py --ics ./life.ics reminders show
+python3 main.py --ics ./life.ics reminders add --name="Drink Water" --time="2021-11-08 08:00:00"
+python3 main.py --ics ./life.ics reminders complete --uid 48aefcff-30b4-4f31-85a1-93692e8fbd5c
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 from os.path import exists
 import click
-from ics import Calendar, Event, DisplayAlarm, Todo
-from datetime import datetime, timedelta
+from ics import Calendar, Event, Todo
+from datetime import datetime, date
 from prettytable import PrettyTable
 import uuid
 
@@ -21,6 +21,24 @@ def cli(ctx, ics):
             calendar = Calendar(ics_file.read())
     else:
         calendar = Calendar()
+        today = datetime.today()
+        meal_1 = Event("Breakfast", datetime(today.year, today.month, today.day, 7), datetime(today.year, today.month, today.day, 7, 30))
+        meal_2 = Event("Lunch", datetime(today.year, today.month, today.day, 12), datetime(today.year, today.month, today.day, 12, 30))
+        meal_3 = Event("Dinner", datetime(today.year, today.month, today.day, 18), datetime(today.year, today.month, today.day, 18, 30))
+        break_1 = Event("Mental Break 1", datetime(today.year, today.month, today.day, 12, 30), datetime(today.year, today.month, today.day, 13))
+        break_2 = Event("Mental Break 2", datetime(today.year, today.month, today.day, 18, 30), datetime(today.year, today.month, today.day, 19))
+        water_1 = Todo(name="Water Reminder 1", due=datetime(today.year, today.month, today.day, 10))
+        water_2 = Todo(name="Water Reminder 2", due=datetime(today.year, today.month, today.day, 15))
+        water_3 = Todo(name="Water Reminder 3", due=datetime(today.year, today.month, today.day, 20))
+        calendar.events.add(meal_3)
+        calendar.events.add(meal_2)
+        calendar.events.add(meal_1)
+        calendar.events.add(break_2)
+        calendar.events.add(break_1)
+        calendar.todos.add(water_1)
+        calendar.todos.add(water_2)
+        calendar.todos.add(water_3)
+
     ctx.obj = {
         "calendar": calendar,
         "ics_path": ics
@@ -31,13 +49,13 @@ def cli(ctx, ics):
 # it will be passing the context wiht the clandear object inside of it to the lower commands such as the show, add and remove and later on more.
 @cli.group()
 @click.pass_context
-def classes(ctx):
-    """ Classes Manager """
+def events(ctx):
+    """ Events Manager (Classes, Chores, Break, Meals) """
     pass
 
 
 # this command print the current classes in the calnedar object in a pretty table format
-@classes.command()
+@events.command()
 @click.pass_obj
 def show(obj):
     x = PrettyTable()
@@ -47,7 +65,7 @@ def show(obj):
     click.echo(x)
         
 # this command add a class to the calendar and save it to the database and take the option flags name begin and end to consturct the class event
-@classes.command()
+@events.command()
 @click.option('--name', required=True)
 @click.option('--begin', type=click.DateTime(), required=True)
 @click.option('--end', type=click.DateTime(), required=True)
@@ -59,7 +77,7 @@ def add(obj, name, begin, end):
 
 
 # this command removes a class with the uid specifcally to it to accurately find the class and remove. Uid used because names are not unique .
-@classes.command()
+@events.command()
 @click.option('--uid', required=True)
 @click.pass_obj
 def remove(obj, uid):
@@ -131,6 +149,59 @@ def remove(obj, uid):
     save_ics(obj['calendar'], obj['ics_path'])
 
 
+@cli.group()
+@click.pass_context
+def chores(ctx):
+    """ Chores """
+    click.echo("chores")
+
+
+@chores.command()
+@click.pass_obj
+def show(obj):
+    x = PrettyTable()
+    x.field_names = ["UID", "Name", "Time", "Completed"]
+    for todo in obj['calendar'].todos:
+        x.add_row([todo.uid, todo.name, todo.dtstamp, todo.completed])
+    click.echo(x)
+
+@chores.command()
+@click.option('--uid', required=True)
+@click.pass_obj
+def complete(obj, uid):
+    todo_to_be_completed = None
+    for todo in obj['calendar'].todos:
+        if todo.uid == uid:
+            todo_to_be_completed = todo
+    if todo_to_be_completed == None:
+        click.echo("Could not find the todo")
+    else:
+        todo_to_be_completed.completed = datetime.now()
+        save_ics(obj['calendar'], obj['ics_path'])
+
+
+@chores.command()
+@click.option('--name', required=True)
+@click.option('--time', type=click.DateTime(), required=True)
+@click.pass_obj
+def add(obj, name, time):
+    t = Todo(name=name, uid=str(uuid.uuid4()), due=time)
+    obj['calendar'].todos.add(t)
+    save_ics(obj['calendar'], obj['ics_path'])
+
+
+@chores.command()
+@click.option('--uid', required=True)
+@click.pass_obj
+def remove(obj, uid):
+    todo_to_be_removed = None
+    for todo in obj['calendar'].todos:
+        if todo.uid == uid:
+            todo_to_be_removed = todo
+    
+    obj['calendar'].todos.remove(todo_to_be_removed)
+    save_ics(obj['calendar'], obj['ics_path'])
+
 # this is the save util ics that will save the infromation from the add command for the clases manager and reminder manager
 # ics is standard that have been improving with time thanks to the giants such as apple and google. To have used another database will be extra uncessary work
 def save_ics(calendar, path):
@@ -139,4 +210,3 @@ def save_ics(calendar, path):
 
 if __name__ == '__main__':
     cli()
-    
